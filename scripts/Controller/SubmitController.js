e89786383a3d71ab4cacb13f1644365763ef2380
Jannik Portz <hello@jannikportz.de>
2015-12-13 16:52:30 +0100

    initial implementation of SubmitController

---
+++ scripts/Controller/SubmitController.js
@@ -0,0 +1,13 @@
+angular.module('petsitting').controller( 'SubmitController', [ '$scope', 'StorageService', function ( $scope, StorageService ) {
+
+  $scope.model = StorageService.getEmptyModel();
+
+  $scope.save = function ( form ) {
+    if ( !form.$valid ) {
+      console.error( 'Form invalid', form );
+      return;
+    }
+    console.log( 'save', $scope.model );
+  };
+
+} ] );

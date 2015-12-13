34d7b36e625d538b361505dc75fb32ab0d27d3b5
Jannik Portz <hello@jannikportz.de>
2015-12-13 16:52:12 +0100

    added empty model to storageservice

--- scripts/Service/StorageService.js
+++ scripts/Service/StorageService.js
@@ -1,5 +1,22 @@
 angular.module('petsitting').service( 'StorageService', [ '$http', '$q', function ( $http, $q ) {
 
+  var emptyModel = {
+    "firstName": "",
+    "lastName": "",
+    "city": "",
+    "zipCode": "",
+    "street": "",
+    "dateStart": "",
+    "dateEnd": "",
+    "animalType": "",
+    "animalBreed": "",
+    "animalName": "",
+    "animalAge": 0,
+    "description": "",
+    "email": "",
+    "phone": ""
+  };
+
   /**
    * Returns a promise resolving with test data from test-data.json
    * @returns     Promise resolving with test data
@@ -18,8 +35,17 @@ angular.module('petsitting').service( 'StorageService', [ '$http', '$q', functio
     return defer.promise;
   };
 
+  /**
+   * Returns an empty model
+   * @returns     object containing all properties with emptyish values
+   */
+  var getEmptyModel = function () {
+    return angular.copy( emptyModel );
+  };
+
   return {
-    getAll: getAll
+    getAll: getAll,
+    getEmptyModel: getEmptyModel
   };
 
 } ] );

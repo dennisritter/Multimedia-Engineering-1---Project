5a866faa5f336a4b9e03e39414ae9f8354935b19
Jannik Portz <hello@jannikportz.de>
2015-12-28 20:58:14 +0100

    minor table improvements

--- scripts/Controller/tableController.js
+++ scripts/Controller/tableController.js
@@ -1,7 +1,12 @@
 angular.module('petsitting').controller('TableController', ['$scope', 'StorageService', function ($scope, StorageService) {
-  $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+  $scope.map = {
+    center: {
+      latitude: 45, longitude: -73
+    },
+    zoom: 8
+  };
 
-  //Saves existent userdata in data attribute
+  // Saves existing user data in data model
   StorageService.getAll().then(function (data) {
     $scope.users = data;
   });

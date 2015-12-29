104ddab29e250ab3b9a4fdd28169986b26ed3dc8
Dennis Ritter <dennis.r.ritter@gmail.com>
2015-12-29 17:35:14 +0100

    changed Google Map to show the users address at the center (only one map working; next maps stay grey)

--- scripts/Controller/tableController.js
+++ scripts/Controller/tableController.js
@@ -1,13 +1,32 @@
-angular.module('petsitting').controller('TableController', ['$scope', 'StorageService', function ($scope, StorageService) {
-  $scope.map = {
-    center: {
-      latitude: 45, longitude: -73
-    },
-    zoom: 8
-  };
+angular.module('petsitting').controller('TableController', ['$scope', 'StorageService', 'uiGmapIsReady', function ($scope, StorageService, uiGmapIsReady) {
 
   // Saves existing user data in data model
   StorageService.getAll().then(function (data) {
     $scope.users = data;
   });
+
+  //uiGmapIsReady.promise(10).then(function(instances){
+  //  instances.forEach(function(instance){
+  //    var map = instance.map;
+  //    var uuid = map.uiGmap_id;
+  //    var mapInstanceNumber = instance.instance; // Starts at 1.
+  //  });
+  //});
+
+  // Loads Google Map centered at the users address
+  $scope.getGoogleMap = function($index){
+    $scope.map = {
+      center: {
+        latitude : $scope.users[$index].latitude,
+        longitude: $scope.users[$index].longitude
+      },
+      zoom: 8
+    };
+  }
+
+  // Toggles details-row for a table entry
+  $scope.toggleDetails = function($index){
+    $scope.activeRow = $scope.activeRow == $index ? -1 : $index;
+  }
+
 }]);

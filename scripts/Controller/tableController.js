7aae539494e90b400054f3eea14fa5502b70e192
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-01-03 16:25:26 +0100

    deleted  unnecessary function

--- scripts/Controller/tableController.js
+++ scripts/Controller/tableController.js
@@ -17,20 +17,11 @@ angular.module('petsitting').controller('TableController', ['$scope', 'StorageSe
     zoom: 13
   };
 
-  // Loads Google Map centered at the users address
-  $scope.getGoogleMap = function($index){
-    $scope.map = {
-      center: {
-        latitude : $scope.users[$index].latitude,
-        longitude: $scope.users[$index].longitude
-      },
-      zoom: 8
-    };
-  };
-
   // Toggles details-row for a table entry
   $scope.toggleDetails = function($index){
     $scope.activeRow = $scope.activeRow == $index ? -1 : $index;
   }
+
+
 
 }]);

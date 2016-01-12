9c96f849cab45cb81cc68aa00b06870d12158b1b
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-01-12 13:19:43 +0100

    Added sql file

---
+++ petsitting.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:8889
+-- Erstellungszeit: 12. Jan 2016 um 13:17
+-- Server-Version: 5.5.42
+-- PHP-Version: 5.6.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `petsitting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `userdata`
+--
+
+CREATE TABLE `userdata` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `firstName` varchar(128) DEFAULT NULL,
+  `lastName` varchar(128) DEFAULT NULL,
+  `city` varchar(128) DEFAULT NULL,
+  `zipCode` smallint(6) DEFAULT NULL,
+  `street` varchar(128) DEFAULT NULL,
+  `dateStart` date DEFAULT NULL,
+  `dateEnd` date DEFAULT NULL,
+  `animalType` varchar(128) DEFAULT NULL,
+  `animalBreed` varchar(128) DEFAULT NULL,
+  `animalName` varchar(128) DEFAULT NULL,
+  `animalAge` double DEFAULT NULL,
+  `description` varchar(512) DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `phone` int(16) DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `userdata`
+--
+ALTER TABLE `userdata`
+  ADD PRIMARY KEY (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

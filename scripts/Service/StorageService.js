c9e9c292eef15f09dc1154cadcfddc56a0f1a187
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-01-18 17:13:54 +0100

    added StorageService methods

--- scripts/Service/StorageService.js
+++ scripts/Service/StorageService.js
@@ -18,33 +18,90 @@ angular.module('petsitting').service( 'StorageService', [ '$http', '$q', '$timeo
   };
 
   /**
-   * Returns a promise resolving with test data from test-data.json
-   * @returns     Promise resolving with test data
-   */
-  var getAll = function () {
-    var defer = $q.defer();
-
-    //$http( {
-    //  url: "/api/api.php",
-    //  method: "GET"
-    //} )
-    $http.get( 'test-data.json' )
-      .success( function ( response ) {
-        defer.resolve( response );
-      } )
-      .error( function ( response ) {
-        defer.reject( response );
-      } );
-
-    return defer.promise;
-  };
-
-  /**
    * Returns an empty model
    * @returns     object containing all properties with emptyish values
    */
   var getEmptyModel = function () {
     return angular.copy( emptyModel );
+  };
+
+  /**
+   * Returns a promise resolving with all userdata stored in the petsitting.userdata table
+   * @returns     Promise resolving with all userdata
+   */
+  var getAll = function () {
+    var defer = $q.defer();
+
+    $http( {
+      url: "/api/index.php",
+      method: "GET"
+    } )
+        .success( function ( response ) {
+          defer.resolve( response );
+        } )
+        .error( function( response ){
+          defer.reject( response );
+        } );
+
+    //$http.get( 'test-data.json' )
+    //  .success( function ( response ) {
+    //    defer.resolve( response );
+    //  } )
+    //  .error( function ( response ) {
+    //    defer.reject( response );
+    //  } );
+    return defer.promise;
+  };
+
+  /**
+   * Returns a promise resolving with the userdata of a user
+   * @param       id    the primary key to return the userdata for
+   * @returns     Promise resolving with userdata of the given id
+   */
+  var getSingle = function (id) {
+    var defer = $q.defer();
+
+    $http( {
+      url: "/api/index.php",
+      method: "GET",
+      params: {
+        id: id
+      }
+    } )
+        .success( function ( response ) {
+          defer.resolve( response );
+        } )
+        .error( function( response ){
+          defer.reject( response );
+        } );
+
+    return defer.promise;
+  };
+
+  /**
+   * Sends an update request with the provided data to the service.
+   * @param     data    The data model to update
+   * @returns           Promise representing the update process
+   */
+  var update = function ( id, data ) {
+    var defer = $q.defer();
+    $http({
+      url:"/api/index.php",
+      method:"PUT",
+      data: data,
+      params:{
+        id: id
+      }
+    })
+        .success( function( response ){
+          defer.resolve( response );
+        } )
+        .error( function( response ){
+          defer.reject( response );
+        } );
+
+
+    return defer.promise;
   };
 
   /**
@@ -55,9 +112,42 @@ angular.module('petsitting').service( 'StorageService', [ '$http', '$q', '$timeo
   var persist = function ( data ) {
     var defer = $q.defer();
 
-    $timeout( function () {
-      defer.resolve( data );
-    }, 4000 );
+    $http({
+      url:"/api/index.php",
+      method:"POST",
+      data: data
+    })
+        .success( function( response ){
+          defer.resolve( response );
+        } )
+        .error( function( response ){
+          defer.reject( response );
+        } );
+
+    return defer.promise;
+  };
+
+  /**
+   * Sends a delete request for the provided id to the service.
+   * @param     id      the primary key of the userdata that is going the be removed
+   * @returns           Promise representing the delete process
+   */
+  var remove = function ( id ) {
+    var defer = $q.defer();
+
+    $http({
+      url:"/api/index.php",
+      method: "DELETE",
+      params: {
+        id: id
+      }
+    })
+        .success(function (response) {
+          defer.resolve(response);
+        })
+        .error(function (response) {
+          defer.reject(response);
+        });
 
     return defer.promise;
   };

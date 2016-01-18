c2598376f5a522458ab14c5941d0180d90fb69e9
Jannik Portz <hello@jannikportz.de>
2016-01-18 22:26:23 +0100

    added table actions

--- scripts/Controller/tableController.js
+++ scripts/Controller/tableController.js
@@ -1,17 +1,20 @@
-angular.module('petsitting').controller('TableController', ['$scope', 'StorageService', function ($scope, StorageService, ngAnimate) {
+angular.module('petsitting').controller('TableController', ['$scope', 'StorageService', function ( $scope, StorageService ) {
 
   $scope.users = [];
 
-  // Loads data into scope
-  StorageService.getAll().then( function (data) {
-    for ( var i = 0; i < data.length; i++ ) {
-      data[i].position = {
-        latitude: data[i].latitude,
-        longitude: data[i].longitude
-      };
-    }
-    $scope.users = data;
-  });
+  var loadData = function () {
+    StorageService.getAll().then( function (data) {
+      for ( var i = 0; i < data.length; i++ ) {
+        data[i].position = {
+          latitude: data[i].latitude,
+          longitude: data[i].longitude
+        };
+      }
+      $scope.users = data;
+    });
+  };
+
+  loadData();
 
   //map object for google-maps
   $scope.map = {
@@ -23,8 +26,22 @@ angular.module('petsitting').controller('TableController', ['$scope', 'StorageSe
     $scope.activeRow = $scope.activeRow == $index ? -1 : $index;
     //Visibility of contact information
     $scope.isVisible = false;
-  }
+  };
 
+  $scope.remove = function ( id ) {
+    if ( !confirm("Wollen Sie diesen Eintrag wirklich löschen?") ) {
+      return;
+    }
 
+    StorageService.remove( id )
+      .then( function () {
+        for ( var i = 0; i < $scope.users.length; i++ ) {
+          if ( $scope.users[i].id == id ) {
+            $scope.users.splice( i, 1 );
+            break;
+          }
+        }
+      } );
+  };
 
 }]);

295d0f49f007a7d8451229229526d8b18b5edcfc
Jannik Portz <hello@jannikportz.de>
2016-01-18 19:36:55 +0100

    added auto-increment feature to id in sql

--- petsitting.sql
+++ petsitting.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
+-- version 4.1.7
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:8889
--- Erstellungszeit: 13. Jan 2016 um 19:20
--- Server-Version: 5.5.42
--- PHP-Version: 5.6.10
+-- Host: localhost
+-- Erstellungszeit: 18. Jan 2016 um 19:20
+-- Server Version: 5.5.33
+-- PHP-Version: 5.5.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Datenbank: `petsitting`
@@ -26,8 +26,8 @@ SET time_zone = "+00:00";
 -- Tabellenstruktur für Tabelle `userdata`
 --
 
-CREATE TABLE `userdata` (
-  `id` int(11) NOT NULL DEFAULT '0',
+CREATE TABLE IF NOT EXISTS `userdata` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `firstName` varchar(64) DEFAULT NULL,
   `lastName` varchar(64) DEFAULT NULL,
   `city` varchar(64) DEFAULT NULL,
@@ -43,18 +43,9 @@ CREATE TABLE `userdata` (
   `email` varchar(64) DEFAULT NULL,
   `phone` varchar(16) DEFAULT NULL,
   `latitude` double DEFAULT NULL,
-  `longitude` double DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indizes der exportierten Tabellen
---
-
---
--- Indizes für die Tabelle `userdata`
---
-ALTER TABLE `userdata`
-  ADD PRIMARY KEY (`id`);
+  `longitude` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
